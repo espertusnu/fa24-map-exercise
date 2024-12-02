cff2580ca7186dc86bd9a70ad3de7ae626279b09
Ellen Spertus <ellen.spertus@gmail.com>
2024-12-01 19:51:14 -0800

    Reorganize code

--- src/mapgame/procedures/Main.kt
+++ src/mapgame/procedures/Main.kt
@@ -2,30 +2,6 @@ package mapgame.procedures
 
 import kotlin.system.exitProcess
 
-fun orchardMeadow() {
-    println("You are at Orchard Meadow.")
-    println("To the east is Warren Olney.")
-    println("To the southeast is GSB.")
-    while (true) {
-        println("Which way do you want to go?")
-        val dir = readln()
-        when (dir) {
-            "southeast" -> run {
-                gsb()
-                return
-            }
-
-            "east" -> run {
-                warrenOlney()
-                return
-            }
-
-            "quit" -> exitProcess(0)
-            else -> println("You can't go that direction.")
-        }
-    }
-}
-
 fun gsb() {
     println("You are at GSB.")
     println("To the northwest is Orchard Meadow.")
@@ -56,14 +32,60 @@ fun gsb() {
     }
 }
 
+fun orchardMeadow() {
+    println("You are at Orchard Meadow.")
+    println("To the east is Warren Olney.")
+    println("To the southeast is GSB.")
+    while (true) {
+        println("Which way do you want to go?")
+        val dir = readln()
+        when (dir) {
+            "southeast" -> run {
+                gsb()
+                return
+            }
+
+            "east" -> run {
+                warrenOlney()
+                return
+            }
+
+            "quit" -> exitProcess(0)
+            else -> println("You can't go that direction.")
+        }
+    }
+}
+
 fun warrenOlney() {
-    TODO("Implement Warren Olney")
+    println("You are at Warren Olney.")
+    println("To the west is Orchard Meadow.")
+    println("To the southwest is GSB.")
+    while (true) {
+        println("Which way do you want to go?")
+        val dir = readln()
+        when (dir) {
+            "southwest" -> run {
+                gsb()
+                return
+            }
+
+            "west" -> run {
+                orchardMeadow()
+                return
+            }
+
+            "quit" -> exitProcess(0)
+            else -> println("You can't go that direction.")
+        }
+    }
+}
+
+fun main() {
+    gsb()
 }
 
 fun richardsRoad() {
     TODO("Implement Richards Road")
 }
 
-fun main() {
-    gsb()
-}
+
